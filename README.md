7926aa5877592594003151e5962c271a1bbebc92
Sampoorna Biswas <msanonymous.nobody@gmail.com>
2017-11-16 13:05:20 -0800

    FEATURE: New feature Title and README updates

--- README.md
+++ README.md
@@ -5,10 +5,26 @@ Observations:
 -------------
 
 1. Using SVM
-   - With rbf kernel
+   - With RBF kernel
    - Gamma = 1 overfits more than 0.1 and 0.01
    - Gamma = 0.001 is better than 1
    - Incorporating grid search to find the best C and gamma
      - The best gamma is consistently 0.001
      - The best C is in the range of 10^3 but varies from 300-500
      - Chosen C = 560 based on best performance on Kaggle
+	 
+   - Using polynomial kernel
+     - Using C = 560 as with RBF kernel
+     - Degree = 3 doesn't perform as well as RBF with params above
+	 - Degree = 5 takes too long to run
+	 
+2. Feature engineering
+   - Normalize titles in names
+     - Convert 'Mlle.' and 'Ms.' to 'Miss.', and 'Mme.' to 'Mrs.'
+	 - Convert some rare titles ('Capt.', 'Col.', 'Rev.') to 'Mr.', which are observed to be only male in this dataset (not being sexist), and would most likely require one to be an adult
+	 - Club others into a separate class 'Other'
+   - Replace NaN values
+     - For Age and Fare, replace by median
+	 - For Embarked, only 3 rows in the training set have NaN here, so we choose 'S'  (Southampton as port of embarkation) arbitrarily.
+   
+	 
--- titanic.py
+++ titanic.py
@@ -10,6 +10,7 @@ import random
 ### Read files
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
+print "Reading input files: COMPLETE"
 
 ### Save passenger IDs to write to file later
 ids = test_df['PassengerId'].tolist()
@@ -27,18 +28,28 @@ test_df.fillna({'Age' : test_df['Age'].median(), 'Embarked' : 'S',
 y = train_df['Survived'].tolist()
 #train_df.drop(['Survived'], axis = 1)
 
-### Replace non-numeric values with numbers
-train_df.loc[train_df['Sex'] == 'male', 'Sex'] = 0
-train_df.loc[train_df['Sex'] == 'female', 'Sex'] = 1
-test_df.loc[test_df['Sex'] == 'male', 'Sex'] = 0
-test_df.loc[test_df['Sex'] == 'female', 'Sex'] = 1
+### Extract titles, replace uncommon variations and add as column to dataframe
+names_train = train_df['Name'].tolist()
+names_test = test_df['Name'].tolist()
+titles = [name.split(',')[1].split(' ')[1] for name in names_train]
+train_df['Title'] = titles
+titles = [name.split(',')[1].split(' ')[1] for name in names_test]
+test_df['Title'] = titles
+train_df.Title.replace(['Ms.', 'Mlle.', 'Mme.'], ['Miss.', 'Miss.', 'Mrs.'], inplace=True)
+train_df.Title.replace(['Dona.', 'Capt.', 'Col.', 'Don.', 'Dr.', 'Jonkheer.', 'Lady.', 'Major.', 'Rev.', 'Sir.', 'the'], ['Other', 'Mr.', 'Mr.', 'Other', 'Other', 'Mr.', 'Other', 'Mr.', 'Mr.', 'Mr.', 'Other'], inplace=True) 
+test_df.Title.replace(['Ms.', 'Mlle.', 'Mme.'], ['Miss.', 'Miss.', 'Mrs.'], inplace=True)
+test_df.Title.replace(['Dona.', 'Capt.', 'Col.', 'Don.', 'Dr.', 'Jonkheer.', 'Lady.', 'Major.', 'Rev.', 'Sir.', 'the'], ['Other', 'Mr.', 'Mr.', 'Other', 'Other', 'Mr.', 'Other', 'Mr.', 'Mr.', 'Mr.', 'Other'], inplace=True)
 
-train_df.loc[train_df['Embarked'] == 'C', 'Embarked'] = 0
-train_df.loc[train_df['Embarked'] == 'S', 'Embarked'] = 1
-train_df.loc[train_df['Embarked'] == 'Q', 'Embarked'] = 2
-test_df.loc[test_df['Embarked'] == 'C', 'Embarked'] = 0
-test_df.loc[test_df['Embarked'] == 'S', 'Embarked'] = 1
-test_df.loc[test_df['Embarked'] == 'Q', 'Embarked'] = 2
+### Replace non-numeric values with numbers
+train_df.Sex.replace(['male', 'female'], [0, 1], inplace=True)
+test_df.Sex.replace(['male', 'female'], [0, 1], inplace=True)
+
+train_df.Title.replace(['Mr.', 'Mrs.', 'Miss.', 'Master.', 'Other'], [0, 1, 2, 3, 4], inplace=True)
+test_df.Title.replace(['Mr.', 'Mrs.', 'Miss.', 'Master.', 'Other'], [0, 1, 2, 3, 4], inplace=True)
+
+train_df.Embarked.replace(['C', 'S', 'Q'], [0, 1, 2], inplace=True)
+test_df.Embarked.replace(['C', 'S', 'Q'], [0, 1, 2], inplace=True)
+print "Initial data processing: COMPLETE"
 
 ### Transform dataframe to lists
 X = train_df[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']].values.tolist()
@@ -54,26 +65,26 @@ X_valid = X[0:int(0.1*len(X))]
 y_valid = y[0:len(X_valid)]
 X = X[(len(X_valid)+1):-1]
 y = y[(len(X_valid)+1):-1]
+print "Train/test split: COMPLETE"
 
 #######################################################################################
 ### Parameter tuning using grid-search
-#C_range = np.logspace(1, 5, 20)
-#gamma_range = np.logspace(-3, 3, 13)
-#print C_range
-#param_grid = dict(C=C_range)
-#cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
-#grid = GridSearchCV(SVC(), param_grid=param_grid, cv=cv)
-#grid.fit(X, y)
-#print "The best parameters are ", grid.best_params_, "with a score of", grid.best_score_
+C_range = np.logspace(1, 5, 20)
+gamma_range = np.logspace(-3, 3, 13)
+print C_range
+param_grid = dict(C=C_range)
+cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+grid = GridSearchCV(SVC(kernel = 'rbf', gamma=0.001), param_grid=param_grid, cv=cv)
+grid.fit(X, y)
+print "The best parameters are ", grid.best_params_, "with a score of", grid.best_score_
 
 ### Build model
 clf = svm.SVC(kernel = 'rbf', gamma = 0.001, C = 560, cache_size=500) # 1 overfits more than 0.1 and 0.01
 clf.fit(X, y)
+print "Model learning: COMPLETE"
 score = clf.score(X, y)
 print score
 predictions = clf.predict(X_pred)
-
-print predictions
 
 write_file = "output_svm.csv"
 with open(write_file, "wb") as output:
@@ -81,4 +92,4 @@ with open(write_file, "wb") as output:
 	writer.writerow(['PassengerId','Survived'])
 	for ind in range(len(predictions)):
 		writer.writerow([ids[ind], predictions[ind]])
-
+print "Writing results to file: COMPLETE"
